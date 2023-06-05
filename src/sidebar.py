41d98cf9371b10be8f472da7a3f9cf061e6e5aa6
Roll20 <cycla4@gmail.com>
2023-06-05 23:30:03 +0800

    v0.2 done

--- src/sidebar.py
+++ src/sidebar.py
@@ -13,7 +13,7 @@ def sidebar():
         st.subheader("Chat with over 250,000 pages of modern & historical philosophy texts")
         st.subheader("与超过25万页古典和现代哲学典籍对话")
         st.subheader("25万ページ以上の現代・歴史哲学のテキストを使ったチャット")
-            
+        st.divider()
         st.markdown(
             "## How to Use\n"
             "1. Submit a philosophical inquiry through the text box💬\n" 
@@ -31,7 +31,7 @@ def sidebar():
         if quick_feedback:
             record_feedback(quick_feedback)
 
-        st.markdown("---")
+        st.divider()
         st.markdown("# About")
         st.markdown(
             "This conversational AI is embedded with over 250,000 pages of modern & traditional philosophical texts. "
@@ -43,6 +43,6 @@ def sidebar():
             "Contributions are welcome on [GitHub](https://github.com/Creative-Ataraxia). "
             "Bug reports and feature requests can also be submitted through Github."
         )
-        st.markdown("---")
+        st.divider()
         st.markdown("Made with :heart: by [Creative_Ataraxia](<https://github.com/Creative-Ataraxia>)")
         
